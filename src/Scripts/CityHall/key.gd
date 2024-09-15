8fb5f2683b92c643eac112f3b89bdd002d932006
Sean Miller <seancm800@gmail.com>
2024-09-15 04:43:03 -0400

    Added sfx for key and health pickup

--- src/Scripts/CityHall/key.gd
+++ src/Scripts/CityHall/key.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
 @onready var pivot = $Pivot
+@onready var pickup_sfx: AudioStreamPlayer3D = $PickupSFX
+@onready var timer: Timer = $Timer
+@onready var omni_light_3d: OmniLight3D = $OmniLight3D
 
 signal picked_up
 
@@ -10,4 +13,11 @@ func _process(delta: float) -> void:
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	picked_up.emit()
+	pivot.set_visible(false)
+	omni_light_3d.light_energy = 0
+	pickup_sfx.play()
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
 	queue_free()
